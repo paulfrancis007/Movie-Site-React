fc99565de882b437e995e5d46af492261a36929f
Paul Francis <paulfrancis.8e@gmail.com>
2026-08-03 17:56:11 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id = "dad">
+        <h1>Hello World</h1>
+        <h3>Welcome to React</h3>
+      </div>
+      <div id = "chahca">
+        <h2>This is a new div</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
