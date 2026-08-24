a8fdd9fbda42a88258a8eb5b8b90c410f3cf231e
Paul Francis <paulfrancis.8e@gmail.com>
2026-08-24 18:59:30 +0530

    Added favourites and functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
-import Home from './pages/Home'
+import "./App.css";
+import Favourites from "./pages/Favourites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App(){
-  return(
+function App() {
+  return (
     <>
-      <Home />
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
     </>
   );
 }
 
-export default App
+export default App;
